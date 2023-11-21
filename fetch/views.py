9923b5258fb54b8c3822b10c5e8ef5d106f511d1
saichethanr <saichethan9019@gmail.com>
2023-11-22 00:10:02 +0530

    productresponse

--- fetch/views.py
+++ fetch/views.py
@@ -8,15 +8,21 @@ class ProductDetailsView(APIView):
     def get(self, request, *args, **kwargs):
         with connection.cursor() as cursor:
             query = """
-             SELECT pd.productid AS product_id, pd.productName AS product_name, 
-                   pd.productDesc AS product_desc, pd.productDate AS product_date, 
-                   pd.productRating AS product_rating, pd.productLife AS product_life, 
-                   pd.productPrice AS product_price, pd.productImg AS product_img,
-                   p.merchantid AS merchant_id
-            FROM public.productdetails pd
-            JOIN public.products p ON pd.productid = p.productid
+          SELECT pd.productid AS product_id, pd."productName" AS product_name, 
+       pd."productDesc" AS product_desc, pd."productDate" AS product_date, 
+       pd."productRating" AS product_rating, pd."productLife" AS product_life, 
+       pd."productPrice" AS product_price, pd."productImg" AS product_img,
+       p.merchantid AS merchant_id
+FROM public.productdetails pd
+JOIN public.products p ON pd.productid = p.productid;
+
             """
             cursor.execute(query)
-            result = cursor.fetchall()
-            serializer = ProductSerializer(result, many=True)
-            return Response(serializer.data)
+            rows = cursor.fetchall()
+            serialized_data = []
+            for row in rows:
+                # Convert each tuple to a dictionary
+                data_dict = dict(zip([col[0] for col in cursor.description], row))
+                serializer = ProductSerializer(data_dict)
+                serialized_data.append(serializer.data)
+            return Response(serialized_data)
---
+++ index.js
@@ -0,0 +1,10 @@
+fetch("http://127.0.0.1:8000/fetch/products/")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
