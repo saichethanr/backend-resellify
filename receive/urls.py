8074e9b367f960b14dd1ef2aebe9e8543d9e9198
saichethanr <saichethan9019@gmail.com>
2023-11-26 13:14:08 +0530

    product view added

--- receive/urls.py
+++ receive/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterUserView,LoginUserView
+from .views import RegisterUserView,LoginUserView,ProductView
 urlpatterns = [
     path('register/',RegisterUserView.as_view()),
-    path('login/',LoginUserView.as_view())
+    path('login/',LoginUserView.as_view()),
+    path('addproduct/',ProductView.as_view())
 ]
--- receive/views.py
+++ receive/views.py
@@ -5,8 +5,6 @@ from rest_framework import status
 from django.contrib.auth import authenticate, login
 from django.db import connection
 import random
-from django.db import connection
-from django.db import connection
 from rest_framework.decorators import api_view,renderer_classes
 from rest_framework.renderers import JSONRenderer
 from django.contrib.auth.hashers import check_password 
@@ -49,9 +47,7 @@ class RegisterUserView(APIView):
 
             cursor.execute("SELECT * FROM merchants WHERE \"merchantMail\"  = %s", [email])
             merchant_exists = cursor.fetchone()
-    
         try:
-            
             if customer_exists or merchant_exists:
                 return Response({'message': 'User already exists'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -81,7 +77,6 @@ def is_valid_merchant_code(merchant_code):
 
 class LoginUserView(APIView):
     renderer_classes = [JSONRenderer]
-
     def post(self, request, *args, **kwargs):
         data = request.data
         mail = data.get('mail')
@@ -119,5 +114,51 @@ class LoginUserView(APIView):
                 })
         return Response({'authenticated': False})
 
+class ProductView(APIView):
+    renderer_classes = [JSONRenderer]
 
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        product_id = generate_unique_product_id()
+        product_name = data.get('product_name')
+        product_desc = data.get('product_desc')
+        product_date = data.get('product_date')
+        product_rating = data.get('product_rating')
+        product_life = data.get('product_life')
+        product_price = data.get('product_price')
+        product_img = data.get('product_img')
+        category_name = data.get('category_name')
+        merchant_id = data.get('merchant_id')
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    SELECT categoryid FROM public.categories
+                    WHERE "categoryName" = %s
+                """, [category_name])
+                result = cursor.fetchone()
+                if result:
+                    category_id = result[0]
+                else:
+                    return Response({"error": f"Category '{category_name}' not found."}, status=status.HTTP_400_BAD_REQUEST)
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    INSERT INTO public.products
+                    VALUES (%s, %s, %s)
+                """, [product_id, 2608, category_id])
+
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    INSERT INTO public.productdetails VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                """, [product_id, product_name, product_desc, product_date, product_rating, product_life, product_price, product_img])
+            return Response({"productadd": "true",'productid':product_id}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+def generate_unique_product_id():
+    while True:
+        product_id = random.randint(4000, 4999)
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT COUNT(*) FROM public.productdetails WHERE productid = %s", [product_id])
+            count = cursor.fetchone()[0]
+        if count == 0:
+            return product_id
 
